5c28d2433865cc7d48bb883d86c53d16db4aa207
Johannes <johannes.nichell@sap.com>
2018-11-20 19:41:16 +0900

    Initial.

---
+++ server.js
@@ -0,0 +1,30 @@
+//var express = require('express');
+//var app = express();
+var fs = require('fs');
+
+//app.use("/public", express.static(__dirname + "/UI/dist"));
+
+// app.get("/api/v1/intro", function (req, res) {
+// 	res.send('Intro');
+// });
+//
+// app.get("/*", function (req, res) {
+// 	response = {
+// 		"status": "404",
+// 		"message": "Access the translation permutator API at /api/v1/intro or the webapp at /static/src/index.html"
+// 	}
+// 	res.json(response);
+// });
+
+// app.listen(3000, function () {
+// 	console.log('Listening on port 3000.');
+// });
+
+
+// Read in file
+
+var sourceFile =
+
+fs.readFile('./source.xml', 'utf8', function(err, contents) {
+    console.log(contents);
+});
